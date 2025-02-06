b0b179b24bc18d251901de4836b72f8db6802a6b
penguineo <subham020304@gmail.com>
2025-02-06 13:43:17 +0000

    refactor: router used instead of http

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { serverSession } from "./middleware";
 import passport from "passport";
+import path from "path";
 
-export const http = express();
+const http = express();
+export const router = express.Router();
 
+http.use(express.static(path.join(process.cwd(), "../frontend/pages")));
+http.use(express.static(path.join(process.cwd(), "../frontend/")));
 http.use(serverSession);
 http.use(passport.initialize());
 http.use(passport.session());
-
 http.listen(process.env.PORT_HTTP, () => {
 	console.log(`Http Server connected on port: ${process.env.PORT_HTTP}`);
 });
