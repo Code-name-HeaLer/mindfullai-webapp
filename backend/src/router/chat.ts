810b62be761794011caa0636886a9b8a1065ff92
penguineo <subham020304@gmail.com>
2025-02-06 14:50:53 +0000

    chore: remove unnecessary code

--- backend/src/router/chat.ts
+++ backend/src/router/chat.ts
@@ -2,8 +2,3 @@ import { router } from "../server";
 router.get("/chat", (req, res) => {
 	res.sendFile("/pages/chat.html");
 });
-router.post("/chat");
-router.post("/chat/session");
-router.get("/chat/session");
-router.post("/chat/session");
-router.delete("/chat/session");
